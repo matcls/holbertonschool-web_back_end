ae2cc47866a30cf48e5f29c4963e11cebf7baec3
Manuel Torres <manueltorresvesga@gmail.com>
2021-11-25 17:36:52 -0500

    Format code

--- 0x0A-i18n/0-app.py
+++ 0x0A-i18n/0-app.py
@@ -1,21 +1,19 @@
 #!/usr/bin/env python3
-"""
-Flask app
-"""
+"""Flask app."""
 from flask import Flask, render_template
-
 
 app = Flask(__name__)
 app.url_map.strict_slashes = False
 
 
-@app.route('/', methods=['GET'])
+@app.route("/", methods=["GET"])
 def hello():
-    """ GET /
+    """GET "/".
+
     Return:
       - Render template
     """
-    return render_template('0-index.html')
+    return render_template("0-index.html")
 
 
 if __name__ == "__main__":
