864ee644ccb3d4aba58974b23877a8f6836146f7
Manuel Torres <manueltorresvesga@gmail.com>
2021-12-07 11:49:07 -0500

    Returns the list of school having a specific topic

---
+++ 0x0D-NoSQL/11-schools_by_topic.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""Return the list of schools with a specific topic."""
+
+
+def schools_by_topic(mongo_collection, topic):
+    """Return the list of schools with a specific topic."""
+    return mongo_collection.find({"topics": topic})
